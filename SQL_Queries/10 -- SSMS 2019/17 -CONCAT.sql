269eb93ff68ca52ebbd582f4a02b2b4c0777f85b
andrewcowannagora <53800500+andrewcowannagora@users.noreply.github.com>
2020-03-21 19:14:09 -0400

    Delete 17 -CONCAT.sql

--- SQL_Queries/10 -- SSMS 2019/17 -CONCAT.sql
+++
@@ -1,7 +0,0 @@
-USE AdventureWorks2017
-GO
-
-SELECT FirstName, LastName,
-	CONCAT(FirstName, ' ', MiddleName, ' ', LastName) AS FullName,
-	CONCAT_WS(' ', FirstName, MiddleName, LastName) AS WithSeperators
-FROM Person.Person;
