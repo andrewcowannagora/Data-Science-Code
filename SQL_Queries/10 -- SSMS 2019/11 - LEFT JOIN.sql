0780e2af990d6e8801a590d6aee7e9f2af396871
andrewcowannagora <53800500+andrewcowannagora@users.noreply.github.com>
2020-03-21 19:08:49 -0400

    Delete 11 - LEFT JOIN.sql

--- SQL_Queries/10 -- SSMS 2019/11 - LEFT JOIN.sql
+++
@@ -1,7 +0,0 @@
-USE AdventureWorks2017
-GO
-
-SELECT Person.BusinessEntityID, Person.PersonType, Person.FirstName, Person.LastName, Employee.JobTitle
-FROM Person.Person
-LEFT JOIN HumanResources.Employee
-ON Person.BusinessEntityID = Employee.BusinessEntityID;
