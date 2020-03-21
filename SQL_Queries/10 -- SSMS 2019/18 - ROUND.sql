a84cffc969831258626069fdd21033780d2c3f43
andrewcowannagora <53800500+andrewcowannagora@users.noreply.github.com>
2020-03-21 19:14:15 -0400

    Delete 18 - ROUND.sql

--- SQL_Queries/10 -- SSMS 2019/18 - ROUND.sql
+++
@@ -1,9 +0,0 @@
-USE AdventureWorks2017
-GO
-
-SELECT BusinessEntityID, SalesYTD,
-	ROUND(SalesYTD, 2) AS Round2,
-	ROUND(SalesYTD, -2) AS RoundHundreds,
-	CEILING(SalesYTD) AS RoundCeiling,
-	FLOOR(SalesYTD) AS RoundFloor
-FROM Sales.SalesPerson;
