f58023538d6a1627dd034ae33f994b2ac342dced
andrewcowannagora <53800500+andrewcowannagora@users.noreply.github.com>
2020-03-21 19:09:05 -0400

    Delete 14 -SUM Exploration.sql

--- SQL_Queries/10 -- SSMS 2019/14 -SUM Exploration.sql
+++
@@ -1,9 +0,0 @@
-USE AdventureWorks2017
-GO
-
-SELECT SalesOrderDetail.ProductID, Product.Name, SUM(SalesOrderDetail.OrderQty) AS TotalQtySold
-FROM Sales.SalesOrderDetail
-INNER JOIN Production.Product
-ON Sales.SalesOrderDetail.ProductID = Production.Product.ProductID
-GROUP BY SalesOrderDetail.ProductID, Product.Name
-ORDER BY TotalQtySold DESC;
