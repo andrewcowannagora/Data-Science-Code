888be49dfed43aea7be434f98bacba32da6b8616
andrewcowannagora <53800500+andrewcowannagora@users.noreply.github.com>
2020-03-21 19:14:25 -0400

    Delete 20 - DATE Formats.sql

--- SQL_Queries/10 -- SSMS 2019/20 - DATE Formats.sql
+++
@@ -1,8 +0,0 @@
-USE AdventureWorks2017
-GO
-
-SELECT BusinessEntityID, HireDate,
-	FORMAT(HireDate, 'dddd, MMMM dd, yyyy') AS FormattedDate,
-	FORMAT(HireDate, 'd-MMM') AS FormattedDate2
-FROM HumanResources.Employee;
-	
