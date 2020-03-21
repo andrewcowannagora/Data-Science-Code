36b005a8520fe158ae67d2267a9f4797fb537d62
andrewcowannagora <53800500+andrewcowannagora@users.noreply.github.com>
2020-03-21 19:12:39 -0400

    Delete Create_Index.sql

--- SQL_Queries/9 -- MISC 1/Create_Index.sql
+++
@@ -1,6 +0,0 @@
--- Creates a nonclustered index on the LastName column of the Landon Hotel's Guests table.
-
-USE LandonHotel;
-
-CREATE NONCLUSTERED INDEX IX_Guests_LastName
-ON dbo.Guests (LastName ASC);
